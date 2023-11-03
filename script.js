fb37936808981e93669f3a3e7b594dd14330c8ed
Josh Bacon <joshdbacon@outlook.com>
2023-11-03 19:56:56 -0300

    added some *spice*

--- script.js
+++ script.js
@@ -3,3 +3,42 @@ function smoothScroll(div) {
         behavior: "smooth"
     });
 }
+
+$(document).on("scroll", async function() {
+    let pageTop = $(document).scrollTop();
+    let pageBottom = pageTop + $(window).height();
+
+    let skills = [
+        $(".skill-react"),
+        $(".skill-js"),
+        $(".skill-html"),
+        $(".skill-css"),
+        $(".skill-flutter"),
+        $(".skill-godot"),
+        $(".skill-python"),
+        $(".skill-java")
+    ];
+
+    let list = $(".skills-list");
+    console.log($(list).position().top);
+    console.log(pageBottom);
+
+    if ($(list).position().top < pageBottom) {
+        for (let s in skills) {
+            $(skills[s]).addClass("visible");
+            await new Promise(r => setTimeout(r, 250));
+        }
+    } else {
+        for (let s in skills) {
+            $(skills[s]).removeClass("visible");
+        }
+    }
+});
+
+$(document).ready(function() {
+    
+})
+
+function baconSlide() {
+    
+}
